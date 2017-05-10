99ed34d0c2dd50f5670d503e3879fc81552b201e
Seaborn Lee <seabornlee.cn@gmail.com>
2017-05-10 12:10:46 +0800

    xbli - refactor

--- bin/qinit.js
+++ bin/qinit.js
@@ -5,11 +5,19 @@ let userArgs = process.argv.slice(2);
 let quizName = userArgs[0];
 
 let exec = require('child_process').exec;
-var child = exec('git clone git@github.com:tws-stack/js-online-quiz-template.git ' + quizName, function(err, stdout, stderr) {
+cloneTemplate(quizName, function(err, stdout, stderr) {
     if (err) throw err;
     console.log(stdout);
-    exec('cd '+quizName+' && git remote set-url origin git@github.com:tws-online-quiz/' + quizName +".git", function(err, stdout, stderr) {
-        if (err) throw err;
-        console.log(stdout);
-    });
-});
+    updateGitRemote(quizName, function(err, stdout, stderr) {
+      if (err) throw err;
+      console.log(stdout);
+    })
+})
+
+function cloneTemplate(quizName, callback) {
+  exec('git clone git@github.com:tws-stack/js-online-quiz-template.git ' + quizName, callback);
+}
+
+function updateGitRemote(quizName, callback) {
+  exec('cd ' + quizName + ' && git remote set-url origin git@github.com:tws-online-quiz/' + quizName +".git", callback);
+}
